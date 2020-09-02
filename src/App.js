c4bf11093e41e950975c902200b7b7a35ec252e5
Kevin Jensen <kevinjensen1133@yahoo.com>
2020-09-02 08:27:59 -0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
+import beachLogo from './img/beachBiteLogo.png';
+import rocasLogo from './img/dosRocas.png';
+import styled from 'styled-components';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <MainContainer>
+      <h1>Hello From App</h1>
+      <img src = {rocasLogo} alt = 'Dos Rocas Logo' />
+      <img src = {beachLogo} alt = 'Beach Bite Logo' />
+    </MainContainer>
   );
 }
 
 export default App;
+
+const MainContainer = styled.div `
+  background: dodgerblue;
+  img {
+    width: 25%;
+    
+  }
+`
